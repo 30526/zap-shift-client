bccb02fdf25f792053a194c3dfff7c170178b05a
Taibur Rahman <taiburrahamanapu@gmail.com>
2026-05-18 22:29:29 +0600

    added refetch funtion from tanstack querry to refresh the fetched data after deleting a parcel

--- src/pages/Dashboard/MyParcel.jsx
+++ src/pages/Dashboard/MyParcel.jsx
@@ -11,7 +11,7 @@ const MyParcel = () => {
   const axiosSecure = useAxiosSecure();
 
   // tanstack query
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["my-parcels", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user.email}`);
@@ -38,6 +38,8 @@ const MyParcel = () => {
         // delete the parcel data from database
         axiosSecure.delete(`/parcels/${id}`).then((res) => {
           if (res.data.deletedCount) {
+            // refresh the fetched data with tanstack refetch function
+            refetch();
             Swal.fire({
               title: "Deleted",
               text: "Your parcel request has been deleted.",
