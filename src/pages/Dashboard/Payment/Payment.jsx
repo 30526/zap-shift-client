55d562d19a5cf65cc8998a1da282e51d941646e2
Taibur Rahman <taiburrahamanapu@gmail.com>
2026-05-19 11:09:47 +0600

    fetched parcel data using tanstack query and added loading component

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,6 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useParams } from "react-router";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Loading from "../../../components/Loading/Loading";
 
 const Payment = () => {
+  const { parcelId } = useParams();
+  const axiosSecure = useAxiosSecure();
+
+  const { isLoading, data: parcel } = useQuery({
+    queryKey: ["parcels", parcelId],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels/${parcelId}`);
+      return res.data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
   return <div>Payment</div>;
 };
 
