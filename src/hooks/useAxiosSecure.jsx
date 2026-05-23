933c4114bdc4e936dd56af4b0fd992c59acf4489
Taibur Rahman <taiburrahamanapu@gmail.com>
2026-05-23 14:58:36 +0600

    added error handling in access token

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,25 +1,42 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import useAuth from "./useAuth";
+import { useLocation } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+  const navigate = useLocation();
 
   // request interceptor
   useEffect(() => {
-    const interceptor = axiosSecure.interceptors.request.use((config) => {
+    const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
       config.headers.Authorization = `Bearer ${user?.accessToken}`;
       return config;
     });
 
-   
+    // interceptor response
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        logout().then(() => {
+          navigate("/login");
+        });
+        console.log("errortaibur  message", error);
+        return Promise.reject(error);
+      },
+    );
 
-    return () => axiosSecure.interceptors.request.eject(interceptor);
-  }, [user]);
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, logout, navigate]);
 
   return axiosSecure;
 };
