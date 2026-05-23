356dfd2eabf4d2aa3ee43aa76d80a1e763e3abdd
Taibur Rahman <taiburrahamanapu@gmail.com>
2026-05-23 13:09:09 +0600

    fetched payments data from batabase to the payment history page

--- src/pages/Dashboard/Payment/PaymentHistory.jsx
+++ src/pages/Dashboard/Payment/PaymentHistory.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentHistory = () => {
-  
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: payments = [] } = useQuery({
+    queryKey: ["pamentHistory", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments?email=${user?.email}`);
+      return res.data;
+    },
+  });
 
-  return <div>PaymentHistory</div>;
+  return <div>PaymentHistory{payments.length}</div>;
 };
 
 export default PaymentHistory;
