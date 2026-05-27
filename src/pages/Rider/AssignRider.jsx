ace3c21334e896982ff3b66f9ea84aea6b137f15
Taibur Rahman <taiburrahamanapu@gmail.com>
2026-05-27 23:54:32 +0600

    fetched parcel data in assign rider page

--- src/pages/Rider/AssignRider.jsx
+++ src/pages/Rider/AssignRider.jsx
@@ -1,7 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const AssignRider = () => {
-  return <div>AssignRider</div>;
+  const axiosSecure = useAxiosSecure();
+
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["parcels", "pending-pickup"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        "/parcels?deliveryStatus=pending-pickup",
+      );
+
+      return res.data;
+    },
+  });
+  console.log(parcels.length);
+  return <div>AssignRider {parcels.length}</div>;
 };
 
 export default AssignRider;
